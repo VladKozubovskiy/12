30b85bc6f2e7658ad9eab30d7bee0e56cd2d7bd7
Vlad Kozubovskiy <valdis95959595@gmail.com>
2026-08-12 20:12:39 +0300

    1 Homwork

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    static String customerName = "Sasha";
+    static int cups0fCoffee = 1;
+    static double cost0fCoffee = 99.9;
+    static boolean tip = true;
+    static char usd = '$';
+
+    public static void main(String[] args) {
+        System.out.println("Dear " + customerName + ",thank you for supporting our business!");
+        System.out.println("Cups of coffee bought: " + cups0fCoffee);
+        System.out.println("Cost of 1 cup of coffee: " + usd + cost0fCoffee);
+        System.out.println("Your total is:" + " " + usd + cost0fCoffee);
+        System.out.println("Tip is included? " + tip);
+    }
 }
