ca71ea6777717b28210ead9c9f2030f6a30b2966
Vlad Kozubovskiy <valdis95959595@gmail.com>
2026-08-16 12:49:34 +0300

    homework

---
+++ src/main/java/Dish.java
@@ -0,0 +1,15 @@
+public class Dish {
+    static void main(String[] args) {
+        String name = "Alex";
+        int ocb1 = 3;
+        double ocb2 = 25.5;
+        double ocb3 = 76.5;
+        boolean ocb4 = false;
+        char ocb5 = '$';
+        System.out.println("Dear " + name + ", thank you for visiting our restaurant!");
+        System.out.println("Dishes ordered: " + ocb1);
+        System.out.println("Cost of 1 dish: " + ocb5 + ocb2);
+        System.out.println("Delivery included? "  + ocb4);
+        System.out.println("Your total is: " + ocb5 +ocb3);
+    }
+}
