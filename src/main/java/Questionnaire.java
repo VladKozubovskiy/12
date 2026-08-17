5d42c4a3cc4657ba048a26e1b8a0dbbae7cb962a
Vlad Kozubovskiy <valdis95959595@gmail.com>
2026-08-17 23:31:46 +0300

    Homework3

---
+++ src/main/java/Questionnaire.java
@@ -0,0 +1,14 @@
+public class Questionnaire {
+    public static void main(String[] args) {
+        String Name = "Emma";
+        String city = "London";
+        int age = 28;
+        double Temperature = 22.5;
+        boolean pet = true;
+        System.out.println("Name: "+ Name);
+        System.out.println("Age: " + age);
+        System.out.println("City: "+ city);
+        System.out.println("Temperature: "+Temperature);
+        System.out.println(" Has pet: "+ pet);
+    }
+}
