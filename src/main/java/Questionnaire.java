f597b1fcf61f20537f020598e16d1d2f5f9cce40
Vlad Kozubovskiy <valdis95959595@gmail.com>
2026-08-21 01:10:36 +0300

    Homework3

--- src/main/java/Questionnaire.java
+++ src/main/java/Questionnaire.java
@@ -1,14 +1,14 @@
 public class Questionnaire {
     public static void main(String[] args) {
-        String Name = "Emma";
+        String name = "Emma";
         String city = "London";
         int age = 28;
         double Temperature = 22.5;
         boolean pet = true;
-        System.out.println("Name: "+ Name);
+        System.out.println("Name: " + name);
         System.out.println("Age: " + age);
-        System.out.println("City: "+ city);
-        System.out.println("Temperature: "+Temperature);
-        System.out.println(" Has pet: "+ pet);
+        System.out.println("City: " + city);
+        System.out.println("Temperature: " + Temperature);
+        System.out.println("Has pet: " + pet);
     }
 }
--- src/main/java/TravelAgency.java
+++ src/main/java/TravelAgency.java
@@ -1,15 +1,15 @@
 public class TravelAgency {
     public static void main(String[] args) {
-        String Name = "Maria";
+        String name = "Maria";
         int days = 5;
         double day = 120.5;
         double day2 = 602.5;
         char usd = '$';
-        boolean inc = false ;
-        System.out.println(" Hello "+ Name +", your trip is booked!");
-        System.out.println(" Number of days: "+ days);
-        System.out.println(" Cost of 1 day: "+usd+ day );
-        System.out.println(" Your total is: "+ usd + day2);
-        System.out.println(" Breakfast included? "+ false);
+        boolean inc = false;
+        System.out.println("Hello " + name + ", your trip is booked!");
+        System.out.println("Number of days: " + days);
+        System.out.println("Cost of 1 day: " + usd + day);
+        System.out.println("Your total is: " + usd + day2);
+        System.out.println("Breakfast included? " + inc );
     }
 }
