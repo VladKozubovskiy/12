b2d636136f61071bd41d0354d504cf64d00913d1
Vlad Kozubovskiy <valdis95959595@gmail.com>
2026-09-05 13:27:58 +0300

    Homework5

---
+++ src/main/java/DeclareVariables.java
@@ -0,0 +1,19 @@
+public class DeclareVariables {
+    static String customerName = "Sasha";
+    static int cupsOfCoffee = 1;
+    static double costOfCoffee = 99.9;
+    static boolean tip = true;
+
+    public static String declareVariables() {
+        String response = "";
+        response = "Dear " + customerName + ", thank you for supporting our business!"
+                + "Cups of coffee bought: " + cupsOfCoffee + ".Cost of 1 cup of coffee: $"
+                + costOfCoffee + ".Your total is: $99.9.Tip is included? " + tip + ".";
+        return response;
+    }
+        public static void main (String[] args){
+            System.out.println(declareVariables());
+
+
+        }
+    }
